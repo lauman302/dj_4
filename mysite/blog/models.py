94f5cfed508e5f783c7ba0108475bb9d595ffafc
Rustam A <69015050+lauman302@users.noreply.github.com>
2022-12-04 18:45:50 +0300

    add new fields, templates, admin

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -1,5 +1,14 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset().filter(status=Post.Status.PUBLISHED)
+
 
 class Post(models.Model):
 
@@ -9,6 +18,10 @@ class Post(models.Model):
 
     title = models.CharField(max_length=250)
     slug = models.SlugField(max_length=250)
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='blog_posts')
     body = models.TextField()
     publish = models.DateTimeField(default=timezone.now)
     created = models.DateTimeField(auto_now_add=True)
@@ -19,11 +32,13 @@ class Post(models.Model):
         default=Status.DRAFT
     )
     
+    objects = models.Manager()
+    published = PublishedManager()
 
     class Meta:
         ordering = ['-publish']
         indexes = [
-            models.Index(fields=['publish'])
+            models.Index(fields=['-publish'])
         ]
 
 
