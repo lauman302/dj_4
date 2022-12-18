98b52ae304bc3d97cc21986f82aa4f02652578e1
Rustam A <69015050+lauman302@users.noreply.github.com>
2022-12-18 17:41:21 +0300

    add search-page

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,11 +1,12 @@
 from django.core.mail import send_mail
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
+from django.contrib.postgres.search import SearchVector, SearchQuery, SearchRank
 from django.shortcuts import get_object_or_404, render
 from django.views.generic import ListView
 from django.db.models import Count
 from django.views.decorators.http import require_POST
 
-from .forms import CommentForm, EmailPostForm
+from .forms import CommentForm, EmailPostForm, SearchForm
 from .models import Comment, Post
 from taggit.models import Tag
 
@@ -94,3 +95,27 @@ def post_comment(request, post_id):
         comment.post = post
         comment.save()
     return render(request, 'blog/post/comment.html', {'post': post, 'form': form, 'comment': comment})
+
+
+def post_search(request):
+    form = SearchForm()
+    query = None
+    results = []
+
+    if 'query' in request.GET:
+        form = SearchForm(request.GET)
+        if form.is_valid():
+            query = form.cleaned_data['query']
+            search_vector = SearchVector('title', 'body', config='russian')
+            search_query = SearchQuery(query, config='russian')
+            results = Post.published.annotate(
+                search=search_vector,
+                rank=SearchRank(search_vector, search_query)
+            ).filter(search=search_query).order_by('-rank')
+    return render(
+        request,
+        'blog/post/search.html',
+        {'form': form, 
+         'query': query, 
+         'results': results}
+    )
