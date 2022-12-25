8fdda4badac858f87b79cdbef5852a0da2e052a6
Rustam A <69015050+lauman302@users.noreply.github.com>
2022-12-25 23:44:01 +0300

    update settings

--- bookmarks/bookmarks/settings.py
+++ bookmarks/bookmarks/settings.py
@@ -26,7 +26,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'social_django'
+    'social_django',
+     'django_extensions'
 ]
 
 MIDDLEWARE = [
